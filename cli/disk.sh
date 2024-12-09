4b0b8c4a19998268ea29b7ecc81160ae296a1acf
nhtlongcs <thanglong.nguyenho27@mail.dcu.ie>
2024-12-09 16:45:42 +0000

    disk progress bar

--- cli/disk.sh
+++ cli/disk.sh
@@ -1,7 +1,15 @@
-# du -h -a --max-depth 0 ./
+# Ensure pv is installed: sudo apt-get install pv
 
 FOLDER_ARG=$1
 if [ -z "$FOLDER_ARG" ]; then
     FOLDER_ARG="./"
 fi
+
+# Use pv to show progress
+# if pv installed 
+if [ -x "$(command -v pv)" ]; then
+    du -lh --max-depth 1 $FOLDER_ARG | pv -l -s $(find $FOLDER_ARG -maxdepth 1 | wc -l) | sort -h -r
+    exit 0
+fi
+
 du -lh --max-depth 1 $FOLDER_ARG | sort -h -r
