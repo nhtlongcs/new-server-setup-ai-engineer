35a8359a03c67572b548d421767bc6228ce587f6
Thang-Long Nguyen-Ho <thanglong.nguyenho27@mail.dcu.ie>
2026-02-28 00:03:42 +0000

    Add script to display GPU usage by node

---
+++ cli/all.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+scontrol show node | awk '
+
+BEGIN {
+    RS="NodeName="
+}
+
+NR>1 {
+
+    # Lấy tên node
+    split($1,a," ")
+    node=a[1]
+
+    # Lấy Gres line
+    if (match($0,/Gres=[^\n]*/)) {
+        gres_line=substr($0,RSTART+5,RLENGTH-5)
+    } else next
+
+    # Lấy AllocTRES line
+    if (match($0,/AllocTRES=[^\n]*/)) {
+        alloc_line=substr($0,RSTART+10,RLENGTH-10)
+    } else alloc_line=""
+
+    # Tách từng GPU
+    n=split(gres_line,gres_list,",")
+
+    for(i=1;i<=n;i++) {
+
+        if (gres_list[i] ~ /^gpu:/) {
+
+            split(gres_list[i],g,":")
+            gpu=g[2]
+            total=g[3]
+
+            used=0
+
+            if (match(alloc_line,"gres/gpu:"gpu"=[0-9]+")) {
+                tmp=substr(alloc_line,RSTART,RLENGTH)
+                split(tmp,x,"=")
+                used=x[2]
+            }
+
+            free=total-used
+
+            printf "%s %s %d %d %d\n", gpu,node,total,used,free
+        }
+    }
+}
+' | sort | awk '
+
+{
+    gpu=$1; node=$2; total=$3; used=$4; free=$5;
+
+    if (gpu != prev) {
+        if (NR>1){
+            printf "--------------------------------------------------\n"
+            printf "TOTAL %-12s TOTAL=%d USED=%d FREE=%d\n\n", prev, sT, sU, sF
+        }
+        sT=0; sU=0; sF=0;
+        printf "\n========== GPU TYPE: %s ==========\n", gpu
+        printf "%-12s %-8s %-8s %-8s\n", "NODE","TOTAL","USED","FREE"
+    }
+
+    printf "%-12s %-8d %-8d %-8d\n", node,total,used,free
+
+    sT+=total
+    sU+=used
+    sF+=free
+    prev=gpu
+}
+END{
+    if(NR>0){
+        printf "--------------------------------------------------\n"
+        printf "TOTAL %-12s TOTAL=%d USED=%d FREE=%d\n", prev, sT, sU, sF
+    }
+}
+'
